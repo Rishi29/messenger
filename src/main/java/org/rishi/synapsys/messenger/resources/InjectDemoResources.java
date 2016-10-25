04de99df46f242af5394f454f8ce86e461a0f592
Rishi29 <me.rishi29@gmail.com>
2016-10-24 20:39:40 -0400

    changes:
    1)InjectDemoResources
    
    getParamsUsingContext() method is added.
    here @context annotation is used to get hold of the param and access it.
    This is used when we might not know the name of params before accessing
    it. Like we might want to fetch each header and to see what is in each
    header
    
    2) Class MessageFilterBeans{} added
    
    this calss is used to put all the query params at one place when its
    seesms messy at the method (getMessage() in this case) of resource class
    (Messageresource in this case).
    
    Now we can access the params that is inside MessageFilterBeans class by
    using its instanc in the method of resources class.

--- src/main/java/org/rishi/synapsys/messenger/resources/InjectDemoResources.java
+++ src/main/java/org/rishi/synapsys/messenger/resources/InjectDemoResources.java
@@ -7,7 +7,10 @@ import javax.ws.rs.HeaderParam;
 import javax.ws.rs.MatrixParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriInfo;
 
 @Path("/injectdemo")
 @Consumes(MediaType.TEXT_PLAIN)
@@ -22,5 +25,18 @@ public class InjectDemoResources {
 		
 		return "MatrixParam Value = "+matrixParam +"  header param = "+headerParam + " cookie Param = " +cookieParam;                           
 
+		
+	
+	}
+	
+	@GET
+	@Path("context")
+	public String getParamsUsingContext(@Context UriInfo uriInfo, @Context HttpHeaders header){
+		
+		String path= uriInfo.getAbsolutePath().toString();
+		String cookies= header.getCookies().toString();
+		return "path : "+path +" Cookies : "+ cookies;
+		
+		
 }
 }
